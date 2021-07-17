8e1834b23990a86030b22636cd7829c8dbfe8006
Ryan Emmans <ryanemmans@gmail.com>
2021-07-16 23:06:03 -0700

    completed question 2

--- app.js
+++ app.js
@@ -31,9 +31,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let product = a * b
+  let string = 'The product of ' + a + ' and ' +  b + ' is ' + product + '.';
+  console.log(string);
+  return [product, string];
 }
 
+testMultiply(5, 9);
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
