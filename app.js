74da46b4c327bf7c5fa36a488b1114636dec4de7
Ryan Emmans <ryanemmans@gmail.com>
2021-07-17 17:04:29 -0700

    completed question 6

--- app.js
+++ app.js
@@ -56,20 +56,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let sum1 = sum(sum(a,b)[0], c)[0];
+  let sum1 = sum(sum(a, b)[0], c)[0];
   // let sum2 = sum(sum1, c)[0];
   let product1 = multiply(multiply(a, b)[0], c)[0];
   // let product2 = multiply(product1, c)[0];
   let string1 = a + ' and ' + b + ' and ' + c + ' sum to ' + sum1 + '.';
   let string2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product1 + '.';
-  // console.log(string1);
-  // console.log(string2);
+  // console.log(string1, string2);
   return [sum1, product1, string1, string2];
   // your previous function sum returns an array. let answer = sum(10, 5)[0];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4, 7, 5);
+// testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -87,11 +86,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sum1 = sum(sumArr [0], sumArr [1]) [0];
+  let total = sum(sum1, sumArr [2]) [0];
+  let string = sumArr + ' was passed in as an array of numbers, and ' + total + ' is their sum.';
+  // console.log(string);
+  return [total, string];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
 // testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -107,8 +109,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+let multArray = [2,3,4];
 
+function multiplyArray(multArr) { //eslint-disable-line
+  let product1 = multiply(multArr [0], multArr [1]) [0];
+  let total = multiply(product1, multArr [2]) [0];
+  let string = 'The numbers ' + multArr + ' have a product of ' + total + '.';
+  // console.log(string);
+  return [total, string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -134,12 +142,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
+// let  = testDynamicArray();
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  let product1 = multiply(dynamicArray [0], dynamicArray[1]) [0];
+  let product2 = multiply(product1, dynamicArray [2]) [0];
+  let product3 = multiply(product2, dynamicArray [3]) [0];
+  let answer = multiply(product3, dynamicArray [4]) [0];
+  let string = 'The numbers ' + dynamicArray + ' have a product of ' + answer + '.';
+  // console.log(string);
+  return [answer, string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
+
+// I was able to get the test to pass, but surely there has to be a more efficient way to do this instead of having all these separate variables
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+
